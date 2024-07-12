f80c301ba8e7d42b293383672341afce8ef4c050
flitzso <flitzso@hotmail.com>
2024-07-12 10:18:04 -0300

    home login register

---
+++ src/pages/Login.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="login">
+    <h1>Login Page</h1>
+    <p>This is the Login page.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LoginPage'
+}
+</script>
+
+<style scoped>
+/* Estilos específicos para o componente Login.vue */
+.login {
+  text-align: center;
+  margin-top: 20px;
+}
+</style>
+
+
+  
